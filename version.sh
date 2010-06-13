b6a6348fd6060f819e8a2d1a2e4f1c42177c329c
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2010-06-13 20:17:23 +0900

    DANGEROUS signature.

--- version.sh
+++ version.sh
@@ -9,7 +9,7 @@ if [ $LOCALVER \> 1 ] ; then
     if git status | grep -q "modified:" ; then
         VER="${VER}M"
     fi
-    VER="$VER $(git rev-list HEAD -n 1 | cut -c 1-7)"
+    VER="${VER}_DANGEROUS $(git rev-list HEAD -n 1 | cut -c 1-7)"
     echo "#define X264_VERSION \" r$VER\""
 else
     echo "#define X264_VERSION \"\""
